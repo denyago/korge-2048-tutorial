208f12a59dffaf1c638fdf8f7fbb39cf4ecf604b
Denys Yahofarov <denyago@gmail.com>
2022-02-20 19:02:53 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+import com.soywiz.korge.gradle.*
+
+buildscript {
+  val korgePluginVersion: String by project
+
+  repositories {
+    mavenLocal()
+    mavenCentral()
+    google()
+    maven { url = uri("https://plugins.gradle.org/m2/") }
+  }
+  dependencies {
+    classpath("com.soywiz.korlibs.korge.plugins:korge-gradle-plugin:$korgePluginVersion")
+  }
+}
+
+plugins {
+  id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
+}
+
+apply<KorgeGradlePlugin>()
+
+korge {
+  id = "name.denyago.game2048"
+// To enable all targets at once
+
+  // targetAll()
+
+// To enable targets based on properties/environment variables
+  // targetDefault()
+
+// To selectively enable targets
+
+  targetJvm()
+  targetJs()
+  targetDesktop()
+  // targetIos()
+  // targetAndroidIndirect() // targetAndroidDirect()
+}
---
+++ src/commonMain/kotlin/main.kt
@@ -0,0 +1,8 @@
+import com.soywiz.korge.*
+import com.soywiz.korim.color.RGBA
+
+suspend fun main() = Korge(
+  width = 480, height = 640, title = "2048",
+  bgcolor = RGBA(253, 247, 240)
+) {
+}
